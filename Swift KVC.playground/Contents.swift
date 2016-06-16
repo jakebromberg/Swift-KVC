044c81a8e1d88aec48aec58af3f2774d58de2b70
Jake Bromberg <jakebromberg@nestlabs.com>
2016-06-15 21:21:45 -0700

    Upgrading to Swift 3

--- Swift KVC.playground/Contents.swift
+++ Swift KVC.playground/Contents.swift
@@ -1,7 +1,5 @@
-import Swift
-
-extension SequenceType {
-	func findMatch(matcher: Self.Generator.Element -> Bool) -> Self.Generator.Element? {
+extension Sequence {
+	func findMatch(_ matcher: (Iterator.Element) -> Bool) -> Iterator.Element? {
 		for element in self where matcher(element) {
 			return element
 		}
@@ -11,27 +9,26 @@ extension SequenceType {
 }
 
 protocol KeyValueCoding {
-	func valueForKeyPath(keyPath : String) -> Any?
+	func valueFor<T>(keyPath: String) -> T?
 }
 
 extension KeyValueCoding {
-	func valueForKeyPath(keyPath : String) -> Any? {
-		
-		let _keyPath : [String] = keyPath.characters.split(".").map { String.init($0) }
+	func valueFor<T>(keyPath: String) -> T? {
+		let keys : [String] = keyPath.characters.split(separator: ".").map(String.init)
 
-		var mirror = Mirror(reflecting: self)
-		var value : Any? = nil
-		for kp in _keyPath {
-			if let (label, val) = mirror.children.findMatch({ $0.label == kp }) {
-				print(label)
-				mirror = Mirror(reflecting: val)
-				value = val
+		var value : Any = self
+
+        for key in keys {
+            let mirror = Mirror(reflecting: value)
+            
+			if let child = mirror.children.findMatch({ $0.label == key }) {
+				value = child.1
 			} else {
 				return nil
 			}
 		}
 		
-		return value
+		return value as? T
 	}
 }
 
@@ -44,4 +41,6 @@ struct A : KeyValueCoding {
 }
 
 let a = A(aaa: A.B(asdf: "HI"))
-a.valueForKeyPath("aaa.asdf")
+if let b : String = a.valueFor(keyPath: "aaa.asdf") {
+    print(b)
+}
